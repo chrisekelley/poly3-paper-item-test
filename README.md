d417d68dae43703de854460ba741902697900de0
Chris E. Kelley <chris@vetula.com>
2018-01-15 12:57:47 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Poly3-paper-input-test
+
+This repo tests the paper-input widget in polymer 3.
+
+It currently throws this error:
+
+```
+GET http://127.0.0.1:8000/node_modules/@polymer/font-roboto/roboto.js net::ERR_ABORTED
+```
+
+The font-roboto repo was not converted completely - it does not offer a font-roboto/roboto.js.
---
+++ src/index.js
@@ -0,0 +1,32 @@
+import {Element as PolymerElement}
+  from "../node_modules/@polymer/polymer/polymer-element.js"
+
+import '../../node_modules/@polymer/paper-item/paper-item-body.js';
+
+class TestApp extends Element {
+  static get template() {
+    return `
+      <div id="test-content">
+      <paper-item-body one-line>
+      <div>Test one-liner</div>
+      </paper-item-body>
+      </div>
+      `
+  }
+
+  static get is() { return 'test-app'; }
+
+  constructor() {
+    super();
+    this.name = '3.0 preview';
+  }
+
+  // properties, observers, etc. are identical to 2.x
+  static get properties() {
+    name: {
+      Type: String
+    }
+  }
+}
+
+customElements.define(TestApp.is, TestApp);
